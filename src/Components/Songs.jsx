a166f700faeb76cf115928e2997f2a1002559739
Raman Boddula <raman_rc001@yahoo.com>
2022-03-12 00:38:02 +0530

    filtering is left

--- src/Components/Songs.jsx
+++ src/Components/Songs.jsx
@@ -2,11 +2,14 @@ import React from 'react';
 import axios from 'axios';
 import {BsPlayCircleFill,BsPauseCircle} from 'react-icons/bs'
 import { BiSkipNext, BiSkipPrevious } from 'react-icons/bi';
+import { Select } from 'antd';
+
 export const Songs = () => {
     const [songs, setSongs] = React.useState([]);
     const [curr, setCurr] = React.useState({});
     const [showPlayer, setShowPlayer] = React.useState(false);
     const audioPlayer = React.useRef();
+    const { Option } = Select;
     React.useEffect(() => {
         axios.get('https://s3-ap-southeast-1.amazonaws.com/he-public-data/studiod9c0baf.json').then((response) => {
             setSongs(response.data);
@@ -19,18 +22,29 @@ export const Songs = () => {
             audioPlayer.current.load();
             audioPlayer.current.play();
         }
-        setShowPlayer(!showPlayer)
+        setShowPlayer(true)
     }
-
+    const handleFilter = (value) => {
+        console.log(value)
+        let result=value === 'AtoZ' ? songs.sort((a, b) => a.song.localeCompare(b.song)) : songs.sort((a, b) => b.song.localeCompare(a.song));
+        setSongs(result)
+        
+    }
     return (
         <>
-           <h1> CokeStudio Songs </h1>
+            <h1> CokeStudio Songs </h1>
+            <Select style={{width:"10em"}} placeholder="sort by alphabetically" onChange={(value)=>handleFilter(value)}>
+                <Option value='AtoZ'>A to Z</Option>
+                <Option value='ZtoA'>Z to A</Option>
+            </Select>
             <div  style={{display:'grid',gridTemplateColumns:'repeat(3,32%)',gridGap:'1%'}}>
                 {songs.map((el) => {
                     return (<div className="prod" onClick={()=>handlePlay(el)}>
                         <img src={el.cover_image} alt={el.song} />
-                        <p>{el.song}</p>
+                        <div className="details">
+                        <h2>{el.song}</h2>
                         <p>{el.artists}</p>
+                       </div>
                     </div>)
                 }) }
             </div>
@@ -47,10 +61,14 @@ export const Songs = () => {
                 </audio>
                 <div className="btns">
                     <BiSkipPrevious onClick={
-                        () => setCurr(songs[songs.indexOf(curr) - 1< 0 ?songs.length-1:songs.indexOf(curr)-1])} style={{ fontSize: '3em', margin: '10px' }} />
+                        () =>
+                        {
+                            handlePlay(songs[songs.indexOf(curr) - 1 < 0 ? songs.length - 1 : songs.indexOf(curr) - 1])
+                        }
+                    } style={{ fontSize: '3em', margin: '10px' }} />
                     <BsPlayCircleFill  onClick={()=>audioPlayer.current.play()} style={{fontSize:'3em', margin:'10px'}} />
                     <BsPauseCircle  onClick={()=>audioPlayer.current.pause()} style={{fontSize:'3em', margin:'10px'}} />
-                    < BiSkipNext  onClick={()=>setCurr(songs[songs.indexOf(curr)+1 >songs.length-1 ? 0 :songs.indexOf(curr)+1])} style={{fontSize:'3em', margin:'10px'}}/>
+                    < BiSkipNext  onClick={()=>handlePlay(songs[songs.indexOf(curr)+1 >songs.length-1 ? 0 :songs.indexOf(curr)+1])} style={{fontSize:'3em', margin:'10px'}}/>
                 </div>
 
             </div>:null}
